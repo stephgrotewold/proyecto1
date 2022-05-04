f3a91308420bd367cfe73d50b184c987f7bfaec2
josuer02 <97801260+josuer02@users.noreply.github.com>
2022-05-04 14:00:21 -0600

    Add files via upload

---
+++ Funciones_profiling.py
@@ -0,0 +1,20 @@
+import cProfile
+from Funciones import *
+from LecturaArchivoTexto import productos
+productos1=productos.copy()
+cProfile.run('sum_cantidad(1702,5,productos)')
+cProfile.run('rest_cantidad(1702,5,productos)')
+cProfile.run('elim_item(1702,productos)')
+cProfile.run('edit_name(1704,"Indita",productos)')
+cProfile.run('edit_precio(1503,40,productos)')
+cProfile.run('edit_cant(1402,30,productos)')
+cProfile.run('elim_item(1303,productos)')
+cProfile.run('search_name("Pollo", productos)')
+cProfile.run('verify_id(1702,productos)')
+cProfile.run('verify_name(1703, 2,productos)')
+cProfile.run('total("XL",2,productos)')
+cProfile.run('saldar("Leche", 5, productos)')
+cProfile.run('search_carrito("Gallo",productos)')
+cProfile.run('search_cant("Pan_de_agua",productos)')
+cProfile.run('positivo(2)')
+reparar(productos1)
