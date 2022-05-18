3f0d59777e1b80246c7f5ce7d95a21811a9dabf7
josuer02 <97801260+josuer02@users.noreply.github.com>
2022-05-18 16:49:45 -0600

    Update Queue_profiling.py

--- Queue_profiling.py
+++ Queue_profiling.py
@@ -1,10 +1,7 @@
 import cProfile
-from Queue import days_between, elim_vencidos, pop_item, vencidos
-from LecturaArchivoTexto import productos
-from Funciones import reparar
-productos1=productos.copy()
-cProfile.run('days_between("2022-05-05 00:00:00")')
-cProfile.run('vencidos(productos)')
-cProfile.run('elim_vencidos(productos)')
-cProfile.run('pop_item(productos)')
-reparar(productos1)
+from Queue import  *
+products=[]
+cProfile.run('enqueue(products, "Esteban", "Zona16", 35351054)')
+cProfile.run('dequeue(products)')
+cProfile.run('IsEmpty(products)')
+cProfile.run('Empty(products)')
