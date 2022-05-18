3ecddf27e73f3a00ca40d4a16bd4f615effccd68
stephgrotewold <79061296+stephgrotewold@users.noreply.github.com>
2022-05-18 16:51:15 -0600

    Update Funciones_profiling.py

--- Funciones_profiling.py
+++ Funciones_profiling.py
@@ -1,14 +1,14 @@
 import cProfile
 from Funciones import *
-from LecturaArchivoTexto import productos
-productos1=productos.copy()
+from LecturaArchivoTexto import Leer
+productos=[]
+Leer(productos)
 cProfile.run('sum_cantidad(1702,5,productos)')
 cProfile.run('rest_cantidad(1702,5,productos)')
 cProfile.run('elim_item(1702,productos)')
 cProfile.run('edit_name(1704,"Indita",productos)')
 cProfile.run('edit_precio(1503,40,productos)')
 cProfile.run('edit_cant(1402,30,productos)')
-cProfile.run('elim_item(1303,productos)')
 cProfile.run('search_name("Pollo", productos)')
 cProfile.run('verify_id(1702,productos)')
 cProfile.run('verify_name(1703, 2,productos)')
@@ -17,4 +17,3 @@ cProfile.run('saldar("Leche", 5, productos)')
 cProfile.run('search_carrito("Gallo",productos)')
 cProfile.run('search_cant("Pan_de_agua",productos)')
 cProfile.run('positivo(2)')
-reparar(productos1)
