a4636f19b48e6c59bc903a500f6c98a8597e7adc
stephgrotewold <79061296+stephgrotewold@users.noreply.github.com>
2022-05-18 17:04:55 -0600

    Update Encrypting.py

--- Encrypting.py
+++ Encrypting.py
@@ -1,9 +1,21 @@
 from cryptography.fernet import Fernet
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-file = open('key.key', 'rb')
-key=file.read()
-file.close()
+password_provided = "contraseña"  # En base a la password se genera la key
+password = password_provided.encode()
+salt = b'\xd6l:S\x10\x1e!\xf8\x05X\xf8\xb4\xce\xb43T'  #Generado utilizando os.urandom(16)
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
 
+key = base64.urlsafe_b64encode(kdf.derive(password)) 
 
 def encriptar(string):
     fernet = Fernet(key)
@@ -15,7 +27,3 @@ def desencriptar(string):
     if(string!="b''"):
         decrypted=fernet.decrypt(string)
         return decrypted.decode()
-
-
-        
-
