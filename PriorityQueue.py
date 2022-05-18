3e162c329a4548142a5132248b7a48ed559ebafa
stephgrotewold <79061296+stephgrotewold@users.noreply.github.com>
2022-05-18 17:00:14 -0600

    Update PriorityQueue.py

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -32,5 +32,41 @@ def heapSort(arr,n):
         arr[i], arr[0] = arr[0], arr[i]  
         heapify(arr, i, 0)
 
+#Revisa si el queue esta vacío
+def IsEmpty(self):
+    return len(self) == 0
 
+#Elimina la primera posicion de la priority queue
+def elim_head():
+    arr=[]
+    Leer(arr)
+    heap(arr)
+    if not IsEmpty(arr):
+        arr.pop(0)
+        actualizar(arr)
 
+#Elimina los primeros elementos de la priority queue, revisando si el pimero esta vencido
+def elim_vencidos():
+    arr=[]
+    Leer(arr)
+    heap(arr)
+    while not IsEmpty(arr) and days_between(str(arr[0]['caducidad']))<0:
+        arr.pop(0)
+    actualizar(arr)
+
+#Imprime los elementos vencidos de la priority queue
+def print_vencidos():
+    arr=[]
+    Leer(arr)
+    heap(arr)
+    cont=0
+    while not IsEmpty(arr) and days_between(str(arr[cont]['caducidad']))<0:
+        print('Nombre: '+ arr[cont]['item_name'] + ' Fecha de caducidad: ' + arr[cont]['caducidad']+ '\n' ) 
+        cont+=1
+    if not IsEmpty(arr):
+        print('No hay ningun item vencido')
+
+#Conversion del array a un heap
+def heap(arr):
+    n = len(arr)
+    heapSort(arr,n)
